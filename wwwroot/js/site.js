c87e694a1433d049eefc5161c0bceca3e6914b16
Ruben Baskaran <rbaskaran92@gmail.com>
2022-07-28 14:12:21 +0200

    added code for getting and setting html elements

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,6 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function OnClickHandler(buttonType) {
-    console.log("You clicked on" + buttonType);
+function OnClickHandler(buttonType) {    
+    switch (buttonType) {
+        case "change color":
+            if (document.getElementById("fullnameLabelDiv").style.backgroundColor == "orange") {
+                document.getElementById("fullnameLabelDiv").style.backgroundColor = "lightblue"
+            } else {
+                document.getElementById("fullnameLabelDiv").style.backgroundColor = "orange"
+            }
+            break;
+        case "change name":            
+            if (document.getElementById("fullnameLabel").innerText == "Ruben Baskaran") {                
+                document.getElementById("fullnameLabel").innerText = "Baskaran Rajakrishnan"
+            } else {                
+                document.getElementById("fullnameLabel").innerText = "Ruben Baskaran"
+            };
+            break;
+    }
 }
